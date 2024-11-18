9ff4eb75ea278ffa1530a64771ef9f3c5b712b8c
Lentinabrilaputri <lentinaabrilaputri.com>
2024-11-18 17:29:41 +0700

    Praktikum 6 Jobsheet 3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,23 +1,26 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserModel;
-use Iluminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
+        // tambah data user dengan Eloquent Model
         $data = [
-            'level_id' => 2,
-            'username' => 'manager_dua',
-            'nama' => 'Manager 2',
-            'password' => Hash::make('12345')
+            // 'username' => 'customer-1',
+            // 'nama' => 'Pelanggan',
+            // 'password' => Hash::make('12345'),
+            // 'level_id' => 4
+            'nama' => 'Pelanggan Pertama',
         ];
-        UserModel::create($data);
+        // UserModel::insert($data);
+        UserModel::where('username', 'customer-1')->update($data);
 
+        // Mengambil semua data user dan menampilkan ke view
         $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,9 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserModel extends Model
 {
+    // protected $table = 'm_user';
+    // protected $primaryKey = 'user_id';
+
+    // protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    use HasFactory;
+
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
 
