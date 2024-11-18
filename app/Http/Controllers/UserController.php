8fe17e6037b5afb756246e6bcefa567139639ebf
Lentinabrilaputri <lentinaabrilaputri.com>
2024-11-19 06:44:31 +0700

    Praktikum 1 Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,14 +11,20 @@ class UserController extends Controller
     {
         // tambah data user dengan Eloquent Model
         $data = [
-            // 'username' => 'customer-1',
-            // 'nama' => 'Pelanggan',
+            // 'username' => 'manager_dua',
+            // 'nama' => 'Manager 2',
             // 'password' => Hash::make('12345'),
-            // 'level_id' => 4
-            'nama' => 'Pelanggan Pertama',
+            // 'level_id' => 2
+
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+            // 'nama' => 'Pelanggan Pertama',
         ];
+        UserModel::create($data);
         // UserModel::insert($data);
-        UserModel::where('username', 'customer-1')->update($data);
+        // UserModel::where('username', 'customer-1')->update($data);
 
         // Mengambil semua data user dan menampilkan ke view
         $user = UserModel::all();
