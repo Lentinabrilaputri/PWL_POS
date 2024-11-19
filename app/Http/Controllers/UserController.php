fbbea9a603b6f2915ddf3f274601e2fdc1be3f05
Lentinabrilaputri <lentinaabrilaputri.com>
2024-11-19 07:16:59 +0700

    Praktikum 2.1 Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,18 +16,21 @@ class UserController extends Controller
             // 'password' => Hash::make('12345'),
             // 'level_id' => 2
 
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
+            // 'username' => 'manager_tiga',
+            // 'nama' => 'Manager 3',
+            // 'password' => Hash::make('12345'),
+            // 'level_id' => 2
             // 'nama' => 'Pelanggan Pertama',
         ];
-        UserModel::create($data);
+        // UserModel::create($data);
         // UserModel::insert($data);
         // UserModel::where('username', 'customer-1')->update($data);
 
         // Mengambil semua data user dan menampilkan ke view
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // $user = UserModel::find(1);
+        // $user = UserModel::where('level_id', 1)->first();
+        $user = UserModel::firstWhere('level_id', 1);
         return view('user', ['data' => $user]);
     }
 }
