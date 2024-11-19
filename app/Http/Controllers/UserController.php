87dcc6ada559bee1a4ea37d20bdb3b338b116c37
Lentinabrilaputri <lentinaabrilaputri.com>
2024-11-19 08:40:03 +0700

    Praktikum 2.3 Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,44 +1,54 @@
 <?php
+
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\UserModel;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            // 'username' => 'manager_dua',
-            // 'nama' => 'Manager 2',
-            // 'password' => Hash::make('12345'),
-            // 'level_id' => 2
+        // Tambah data user dengan Eloquent Model
+        // $data = [
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ];
+        // UserModel::create($data); // Tambahkan data ke tabel m_user
 
-            // 'username' => 'manager_tiga',
-            // 'nama' => 'Manager 3',
-            // 'password' => Hash::make('12345'),
-            // 'level_id' => 2
-            // 'nama' => 'Pelanggan Pertama',
-        ];
-        // UserModel::create($data);
-        // UserModel::insert($data);
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
         // UserModel::where('username', 'customer-1')->update($data);
 
-        // Mengambil semua data user dan menampilkan ke view
-        // $user = UserModel::all();
+        
         // $user = UserModel::find(1);
-        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
         // $user = UserModel::firstWhere('level_id', 1);
-        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOr(1, ['username', 'nama'], function (){
         //     abort(404);
         // });
-        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOr(20, ['username', 'nama'], function (){
         //     abort(404);
         // });
+        // return view('user', ['data' => $user]);
+
         // $user = UserModel::findOrFail(1);
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
         return view('user', ['data' => $user]);
     }
 }
