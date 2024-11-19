eb1cb069654c3f30c7666b434140c4ce38be217c
Lentinabrilaputri <lentinaabrilaputri.com>
2024-11-19 09:36:14 +0700

    Praktikum 2.4 Jobsheet 4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -15,6 +15,6 @@ class UserModel extends Model
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['level_id', 'username', 'nama'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
 
